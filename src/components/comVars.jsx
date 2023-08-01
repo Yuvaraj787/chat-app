73a57bae4d7dd08e8629cd20a3e5fa8154861cee
YUVARAJ787 <yuvarajv787@gmail.com>
2023-08-01 20:11:37 +0530

    Production and development mode detected

--- src/components/comVars.jsx
+++ src/components/comVars.jsx
@@ -1,4 +1,5 @@
-const ApiUrl = "https://chat-app-backend-pp9x.onrender.com";
-// const ApiUrl = "http://localhost:3000";
+const ApiUrl =  process.env.NODE_ENV == "production" ? 
+                "https://chat-app-backend-pp9x.onrender.com" : 
+                "http://localhost:3000";
 
 export { ApiUrl }
