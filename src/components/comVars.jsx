410e4e9fad10ed900f7f77efe4bf0631c747512c
YUVARAJ787 <yuvarajv787@gmail.com>
2023-08-01 11:11:23 +0530

    url changed

--- src/components/comVars.jsx
+++ src/components/comVars.jsx
@@ -1,4 +1,4 @@
-const Apiurl = "https://chat-app-backend-pp9x.onrender.com";
+const ApiUrl = "https://chat-app-backend-pp9x.onrender.com";
 // const ApiUrl = "http://localhost:3000";
 
 export { ApiUrl }
